9b3016c31a645fb5eee57c54cf512d7131681b25
Atharv <atharv@Atharvs-MacBook-Air.local>
2024-03-06 02:36:33 +0530

    db connection successfull

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const dbConnect = async()=>{
+    try {
+       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+       console.log("database connected succesfully");
+    } catch (error) {
+        console.log("ERROR while connecting to Database: ", error.message);
+        
+    }
+}
+
+export {dbConnect};
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { dbConnect } from "./db/index.js";
+import dotenv from 'dotenv'
+
+dotenv.config({
+    path: './.env'
+})
+
+dbConnect();
